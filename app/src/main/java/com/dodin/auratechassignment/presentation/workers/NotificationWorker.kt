394084ec725e43cb11e5a07cf80f6b4f8c7eb940
Eugene Dodin <eugene.dodin@branderstudio.com>
2023-07-01 13:24:46 +0300

    Create notification with special text

--- app/src/main/java/com/dodin/auratechassignment/presentation/workers/NotificationWorker.kt
+++ app/src/main/java/com/dodin/auratechassignment/presentation/workers/NotificationWorker.kt
@@ -6,20 +6,24 @@ import android.app.NotificationManager
 import android.content.Context
 import android.os.Build
 import androidx.core.app.NotificationCompat
+import androidx.work.CoroutineWorker
 import androidx.work.ExistingPeriodicWorkPolicy
 import androidx.work.PeriodicWorkRequestBuilder
 import androidx.work.WorkManager
-import androidx.work.Worker
 import androidx.work.WorkerParameters
 import com.dodin.auratechassignment.R
+import com.dodin.auratechassignment.usecase.GetNotificationTextUseCase
 import java.util.concurrent.TimeUnit
 
 class NotificationWorker(appContext: Context, workerParams: WorkerParameters) :
-    Worker(appContext, workerParams) {
+    CoroutineWorker(appContext, workerParams) {
 
-    override fun doWork(): Result {
+    private lateinit var getNotificationTextUseCase: GetNotificationTextUseCase
+
+    override suspend fun doWork(): Result {
         createNotificationChannel()
-        createNotification()
+        val text = getNotificationTextUseCase()
+        createNotification(text)
 
         return Result.success()
     }
@@ -27,10 +31,8 @@ class NotificationWorker(appContext: Context, workerParams: WorkerParameters) :
     private fun createNotificationChannel() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             val importance = NotificationManager.IMPORTANCE_DEFAULT
-            val name = "Boot Notification"
-            val descriptionText = "Boot Notifications Channel"
-            val channel = NotificationChannel(CHANNEL_ID, name, importance).apply {
-                description = descriptionText
+            val channel = NotificationChannel(CHANNEL_ID, CHANNEL_NAME, importance).apply {
+                description = CHANNEL_DESCRIPTION
             }
             // Register the channel with the system
             val notificationManager =
@@ -39,17 +41,19 @@ class NotificationWorker(appContext: Context, workerParams: WorkerParameters) :
         }
     }
 
-    private fun createNotification(): Notification {
+    private fun createNotification(text: String): Notification {
         val builder = NotificationCompat.Builder(applicationContext, CHANNEL_ID)
             .setSmallIcon(R.drawable.ic_launcher_foreground)
-            .setContentTitle("123")
-            .setContentText("321")
+            .setContentTitle("Reboot events since last notification")
+            .setContentText(text)
             .setPriority(NotificationCompat.PRIORITY_DEFAULT)
         return builder.build()
     }
 
     companion object {
         private const val CHANNEL_ID = "boot-notifications"
+        private const val CHANNEL_NAME = "Boot Notification"
+        private const val CHANNEL_DESCRIPTION = "Boot Notifications Channel"
         private const val WORKER_INTERVAL_MIN = 15L
 
         fun scheduleWorker(context: Context) {
