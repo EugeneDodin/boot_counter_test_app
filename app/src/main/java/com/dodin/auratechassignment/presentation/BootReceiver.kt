4d0e7898cb67e75e558195d5ad8495a6a48b1a7b
Eugene Dodin <eugene.dodin@branderstudio.com>
2023-07-01 13:25:07 +0300

    Schedule worker on reboot event

--- app/src/main/java/com/dodin/auratechassignment/presentation/BootReceiver.kt
+++ app/src/main/java/com/dodin/auratechassignment/presentation/BootReceiver.kt
@@ -3,9 +3,11 @@ package com.dodin.auratechassignment.presentation
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.util.Log
+import com.dodin.auratechassignment.presentation.workers.NotificationWorker
 
 class BootReceiver : BroadcastReceiver() {
     override fun onReceive(context: Context, intent: Intent) {
-
+        NotificationWorker.scheduleWorker(context)
     }
 }
